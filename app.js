5ac3fd7371ed51d0fea3d03037ace9ea092a28d5
GL553VD <GL553VD@FX553VD-DM585T>
2020-08-23 12:41:44 +0300

    ok1

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const userNameRouter = require('./route/userName_R');
+const emailRouter = require('./route/email_R');
+const regRouter = require('./route/reg_R');
+const loginRouter = require('./route/login_R');
+const logoutRouter = require('./route/logout_R');
+const articleRouter = require('./route/article_R');
+const db = require('./db/db_DB');
+
+const app = express();
+async function start()
+{
+  await db.run();
+  app.listen(3000, '192.168.1.100');
+  console.log('The WebServer is listening on localhost:3000');
+}
+start();
+
+app.use((req, res, next) =>
+{
+  console.log('request was made:');
+  console.log('baseUrl: ', req.baseUrl);
+  console.log('remoteAddress: ', req.connection.remoteAddress);
+  console.log('hostname: ', req.hostname);
+  console.log('ip: ', req.ip);
+  console.log('originalUrl: ', req.originalUrl);
+  console.log('params: ', req.params);
+  console.log('path: ', req.path);
+  console.log('protocol: ', req.protocol);
+  console.log('secure: ', req.secure);
+  next();
+});
+
+app.use(express.json());
+
+app.use('/checkusername', userNameRouter);
+app.use('/checkemail', emailRouter);
+app.use('/reg', regRouter);
+app.use('/login', loginRouter);
+app.use('/logout', logoutRouter);
+app.use('/article', articleRouter);
---
+++ controller/article_C.js
@@ -0,0 +1,36 @@
+const Article = require('../db/article_DB');
+const LogIn = require('../db/login_DB');
+
+const write = async (req, res)=>
+{
+  const data = req.body;
+  const title = data["title"];
+  const body = data["body"];
+  const author = data["author"];
+  const writingTimeUTC = data["writingTimeUTC"];
+  const isPublic = data["isPublic"];
+  const result = Article.save(title, body, author, writingTimeUTC, isPublic);
+  res.send(result);
+}
+
+const all = async (req, res)=>
+{
+  const data = req.body;
+  const userName = data["userName"];
+  const passWord = data["passWord"];
+  const skipNumber = data["skipNumber"];
+  const limitNumber = data["limitNumber"];
+  const time = data["time"];
+  //const time2 = new Date(time);
+  //const time3 = time;
+  const isLogin = await LogIn.checkLogin(userName, passWord);
+  if(isLogin)
+  {
+    Article.read(res, skipNumber, limitNumber, time);
+  }
+}
+
+module.exports = {
+  write,
+  all,
+}
